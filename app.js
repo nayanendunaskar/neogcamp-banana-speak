c24feff9c193b83b1fe52275014313f48efa7bd4
nayanendu naskar <nayan19981@gmail.com>
2022-08-15 22:55:46 +0530

    done

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL (text) {
+    return serverURL +"?" + "text="+ text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with the server!! try again after sometime later")
+}
+
 
 function clickHandler() {
-    outputDiv.innerText ="nayanendu " + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
