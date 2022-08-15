de029e819edb734e12ac8a03e9c7f67b2649cb0b
nayanendu naskar <nayan19981@gmail.com>
2022-08-16 00:08:09 +0530

    minor change change

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function clickHandler() {
     .then(response => response.json())
     .then(json => {
         var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
+        outputDiv.innerText = translateText;
        })
     .catch(errorHandler)
 };
